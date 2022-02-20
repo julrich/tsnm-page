5e150933730694d8dc3e631e5cbb41b726608c95
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2022-02-20 17:06:31 +0100

    fix: logo display

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import '../styles/logo.css';
 import '@backlight-dev/selection-inventory-n5vl9.blank-kztxz1qm/style.css';
 
 import type { AppProps } from 'next/app'
