b67cf6d1548e65be68f3a664e73e5cef1423c68a
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2022-02-20 17:36:14 +0100

    chore: add some debugging logs

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,6 +45,8 @@ export async function getServerSideProps({ req }: any) {
     { accessToken: accessToken }
   );
 
+  console.log('getServerSideProps', errors, data);
+
   return {
     props: {
       data,
