fc2213998a0262ad0210318a0cca7332810eb470
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2022-02-26 17:38:41 +0100

    fix: add missing (empty) type for Section

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,3 +1,4 @@
 declare module "@backlight-dev/selection-inventory-n5vl9.tsnm-ds";
 declare module "@backlight-dev/selection-inventory-n5vl9.tsnm-ds/button/dist/Button.js";
 declare module "@backlight-dev/selection-inventory-n5vl9.tsnm-ds/teaser-box/dist/TeaserBox.js";
+declare module "@backlight-dev/selection-inventory-n5vl9.tsnm-ds/section/dist/Section.js";
